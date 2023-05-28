1c5726323983ace41d597dcae7f7eac7443843ff
JakeSchroder <jake.schroder@gmail.com>
2023-05-28 12:38:07 -0700

    Create scroll effect

--- src/app/components/workRolodex.js
+++ src/app/components/workRolodex.js
@@ -2,47 +2,44 @@
 import anime from 'animejs/lib/anime.es.js';
 import { useEffect, useState } from 'react';
 
+let workTitles = [];
+for(let i=0; i<=30; i++){
+    workTitles.push(`project ${i}`)
+}
+
 export default function WorkRolodex(){
     const [scrollPosition, setScrollPosition] = useState(0);
 
     useEffect(()=>{
         anime({
             targets: 'div',
-            translateX: scrollPosition,
-            backgroundColor: 'green',
-            duration: 800
+            duration: 0
           });
     }, [scrollPosition])
 
     const handleScroll = () => {
         const position = event.deltaY;// Might need to be replaced later since event is deprecated
-        setScrollPosition(prev=>Math.abs(prev+position));
+        setScrollPosition(prev=>Math.min(Math.abs(prev+position), 8000));// Might have to be made scalable to the users screen later
     };
-    
     useEffect(() => {
         window.addEventListener('wheel', handleScroll, { passive: false });
-    
         return () => {
             window.removeEventListener('wheel', handleScroll);
         };
-    }, []);
-
-    console.log(scrollPosition)
-    
+    }, []);    
     
     return(
         <div className="grid">
             <text>{scrollPosition}</text>
-            <text className="grid-item">Project 1</text>
-            <text className="grid-item">Project 2</text>
-            <text className="grid-item">Project 3</text>
-            <text className="grid-item">Project 4</text>
-            <text className="grid-item">Project 5</text>
-            <text className="grid-item">Project 6</text>
-            <text className="grid-item">Project 7</text>
-            <text className="grid-item">Project 8</text>
-            <text className="grid-item">Project 9</text>
-            <text className="grid-item">Project 10</text>
+            {workTitles.slice(Math.min(workTitles.length-2, Math.floor(scrollPosition/250)), Math.min(workTitles.length-1, Math.ceil(scrollPosition/250)+6)).map((currentTitle, key) =>{// Simplify later on
+                let opacity = 1/key;
+                let fontSize = 8-key;
+                let paddingBottom = 6;
+                
+                return(
+                    <h1 key={key} className="text-6xl font-extrabold text-slate-100" style={{opacity:opacity, fontSize:`${fontSize}vw`, paddingBottom:paddingBottom-key}} >{currentTitle}</h1>
+                )
+            })}
         </div>
     )
 }
