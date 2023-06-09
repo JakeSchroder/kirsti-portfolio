9124dfd250682f8210dd3c8caa189654865c8f0a
JakeSchroder <jake.schroder@gmail.com>
2023-06-08 17:56:43 -0700

    Convert from animejs to dom manipulation

--- src/app/components/workRolodex.js
+++ src/app/components/workRolodex.js
@@ -1,48 +1,54 @@
 'use client';
-import anime from 'animejs/lib/anime.es.js';
 import { useEffect, useState } from 'react';
 
-let workTitles = [];
-for(let i=0; i<=30; i++){
-    workTitles.push(`project ${i}`)
-}
+let workTitles = [
+    {name: 'playboy', genre: 'wardrobe styling', img_path: ''},
+    {name: 'lil dicky', genre: 'tv and film', img_path: ''},
+    {name: 'darwins playhouse', genre: 'costume design', img_path: ''},
+    {name: 'hbo', genre: 'tv and film', img_path: ''},
+    {name: 'fx', genre: 'tv and film', img_path: ''},
+    {name: 'key art', genre: 'tv and film', img_path: ''},
+    {name: 'jesse mccartney', genre: 'wardrobe styling', img_path: ''},
+    {name: 'allison brooke evans', genre: 'wardrobe styling', img_path: ''},
+    {name: 'lolahika', genre: 'wardrobe styling', img_path: ''},
+    {name: 'jp saxe', genre: 'wardrobe styling', img_path: ''},
+    {name: 'strays', genre: 'tv and film', img_path: ''},
+    {name: 'assistant work', genre: 'tv and film', img_path: ''},
+];
+let titleHeight = 92;// This is the current size of the standard work title element
 
 export default function WorkRolodex(){
     const [scrollPosition, setScrollPosition] = useState(0);
 
     useEffect(()=>{
-        anime({
-            targets: 'div',
-            duration: 0
-          });
+        titleHeight = document.getElementById('0').offsetHeight;
+
     }, [scrollPosition])
 
+
+    // Track how far the user has scrolled
     const handleScroll = () => {
         const position = event.deltaY;// Might need to be replaced later since event is deprecated
-        setScrollPosition(prev=>Math.min(Math.abs(prev+position), 8000));// Might have to be made scalable to the users screen later
+        setScrollPosition(prev=>Math.min(Math.abs(prev+position), 4000));// Might have to be made scalable to the users screen later
     };
     useEffect(() => {
-        window.addEventListener('wheel', handleScroll, { passive: false });
+        document.getElementById('work-title-list').addEventListener('wheel', handleScroll, { passive: false });
         return () => {
-            window.removeEventListener('wheel', handleScroll);
+            //document.getElementById('work-title-list').removeEventListener('wheel', handleScroll);
         };
     }, []);    
     
 
-    let minWorkIndex = Math.min(workTitles.length-2, Math.floor(scrollPosition/250));
-    let maxWorkIndex = Math.min(workTitles.length-1, Math.ceil(scrollPosition/250)+6);
     return(
-        <div className="grid">
-            <text>{scrollPosition}</text>
-            {workTitles.slice(minWorkIndex, maxWorkIndex).map((currentTitle, key) =>{// Simplify later on
-                let opacity = 1/key;
-                let fontSize = 8-key;
-                let paddingBottom = 6;
-                
-                return(
-                    <h1 key={key} className="text-6xl font-extrabold text-slate-100" style={{opacity:opacity, fontSize:`${fontSize}vw`, paddingBottom:paddingBottom-key}} >{currentTitle}</h1>
-                )
-            })}
+        <div className='flex justify-left'>
+            <p className='text-4xl  px-12'>see more of</p>
+            <div id='work-title-list' className=" overflow-scroll snap-y snap-mandatory bg-green-700 h-96 ">
+                {workTitles.concat(workTitles).map((currentTitle, key) =>{// Simplify later on                    
+                    return(
+                        <h1 key={key} id={key} className="text-6xl font-extrabold pb-9 snap-start select-none" >{currentTitle.name}</h1>
+                    )
+                })}
+            </div>
         </div>
     )
 }
