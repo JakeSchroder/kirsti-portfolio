9f342957ff06d6e916fb4d3942a966eb89d77cf1
JakeSchroder <jake.schroder@gmail.com>
2023-06-14 15:21:31 -0500

    Add base for work hero image

--- src/app/components/workHero.js
+++ src/app/components/workHero.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import hero from "../../../public/hero.png"
+
 export default function WorkHero(){
 
 
     return(
-        <div>
-            <Image src={hero} width={40} height={40}></Image>
+        <div className="">
+            <div className="justify-center align-middle">
+                <Image id="image-halo" alt="halo effect behind image related to the project" src={hero} className=" blur-3xl opacity-60 absolute w-6/12 px-12 "></Image>
+                <Image id="work-image" alt="image related to the project" src={hero} className=" absolute w-5/12 px-12" ></Image>
+            </div>
         </div>
     )}
